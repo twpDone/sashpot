067c59af2f29dbbb14e36b321930f31859c2fb7b
root <root@localhost.localdomain>
2016-03-01 11:41:54 +0100

    Simple SSHServer which refuse connection and print the passwd try

---
+++ honey.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+
+import socket
+import paramiko
+import sys
+import signal
+
+IP=""
+PORT=22
+TIMEOUT=15
+
+try:
+    if sys.argv[1]:
+        IP=str(sys.argv[1])
+except:
+    pass
+
+try:
+    if sys.argv[2]:
+        PORT=int(sys.argv[2])
+except:
+    pass
+
+def signal_handler(signal, frame):
+    print('You pressed Ctrl+C!')
+    try:
+        t.close()
+        client.close()
+    except:
+        pass
+    try:
+        sock.close()
+    except:
+        pass
+    
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+host_key = paramiko.RSAKey(filename='/root/.ssh/id_rsa')
+
+class Server (paramiko.ServerInterface):
+    def _init_(self):
+        self.event = threading.Event()
+    def check_channel_request(self, kind, chanid):
+        if kind == 'session':
+            return paramiko.OPEN_SUCCEEDED
+        return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
+    def check_auth_password(self, username, password):
+        print("Try : "+username+"/"+password)
+        if (username == 'root') and (password == 'toor'):
+            return paramiko.AUTH_FAILED
+            #return paramiko.AUTH_SUCCESSFUL
+        return paramiko.AUTH_FAILED
+    def check_channel_pty_request(self, channel, term, width, height, pixelwidth, pixelheight, modes):
+        return False
+    def check_channel_shell_request(self, channel):
+        return False
+def runServer(IP,PORT):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((IP, PORT))
+        print("[*] Running on "+str(IP)+":"+str(PORT))
+        sock.listen(100)
+        print('[+] Listening for connection ...')
+    except Exception, e:
+        print('[-] Listen/bind failed: ' + str(e))
+        sock.close()
+        sys.exit(1)
+    return sock
+
+def getAClient(sock): 
+    try:
+        client, addr = sock.accept()
+        print("[+] Connection from "+str(addr)) 
+    except Exception, e:
+        print('[-] Accept failed: ' + str(e))
+        sock.close()
+        sys.exit(1)
+
+    try:
+        t = paramiko.Transport(client)
+        t.local_version="SSH-2.0-OpenSSH_6.7p1 Debian-5+deb8u1"
+        try:
+            t.load_server_moduli()
+        except:
+            print('[-] (Failed to load moduli -- gex will be unsupported.)')
+            raise
+        t.add_server_key(host_key)
+        server = Server()
+        try:
+            t.start_server(server=server)
+        except paramiko.SSHException, x:
+            print('[-] SSH negotiation failed.')
+
+        chan = t.accept(TIMEOUT)
+        #print('[+] SSH client Connected !')
+        try:
+            dfordata=chan.recv(2024)
+            print(str(dfordata))
+            chan.send('Yeah i can see this')
+        except AttributeError as ex:
+            print("[ ] Time out")
+            t.close()
+    except socket.error as e:
+        print('[-] Client connected but socket was closed')
+        try:
+            t.close()
+            client.close()
+        except:
+            pass
+    except Exception, e:
+        print('[-] Caught exception: '+str(type(e))+ str(e))
+        try:
+            t.close()
+            client.close()
+            sock.close()
+        except:
+            pass
+
+sock=runServer(IP,PORT)
+while 1:
+    getAClient(sock)
+
+t.close()
+client.close()
+sock.close()
